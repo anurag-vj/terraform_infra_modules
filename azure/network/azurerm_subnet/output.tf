066f773749f1c7b7298fa1e9c628dd6cd70b2e66
Anurag Vijay <anurag_vijay@Anurags-MacBook-Air.local>
2026-01-15 01:34:52 +0530

    id output added in azurerm_subnet module

--- azure/network/azurerm_subnet/output.tf
+++ azure/network/azurerm_subnet/output.tf
@@ -2,5 +2,6 @@ output "subnet_ids" {
   value = {
     name             = azurerm_subnet.subnet.name
     address_prefixes = azurerm_subnet.subnet.address_prefixes
+    id               = azurerm_subnet.subnet.id
   }
 }
